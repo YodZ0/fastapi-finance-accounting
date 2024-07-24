7479bde2ffcee0bb72a95b81247a0a5dedc87e52
Timur YodZ0 <tima-mustafa@mail.ru>
2024-07-24 15:46:10 +0600

    Add naming_convention for database

--- backend/core/config.py
+++ backend/core/config.py
@@ -10,6 +10,7 @@ class RunConfig(BaseModel):
 
 class APIPrefix(BaseModel):
     prefix: str = '/api'
+    tags: list = ['Finance-accounting']
 
 
 class DataBaseConfig(BaseModel):
@@ -18,6 +19,13 @@ class DataBaseConfig(BaseModel):
     echo_pool: bool = False
     max_overflow: int = 10
     pool_size: int = 50
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
