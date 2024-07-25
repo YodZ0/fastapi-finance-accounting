e4b33746b6aeb183a6dde54a221c4528ff991ef3
Timur YodZ0 <tima-mustafa@mail.ru>
2024-07-25 12:51:27 +0600

    Add int_id_pk mixin to operations db model

--- backend/core/models/operations.py
+++ backend/core/models/operations.py
@@ -2,9 +2,10 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Numeric, Date
 
 from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
 
 
-class ORMOperation(Base):
+class ORMOperation(IntIdPkMixin, Base):
     title: Mapped[str]
     amount: Mapped[float] = mapped_column(Numeric(10, 2))
     currency: Mapped[str]
