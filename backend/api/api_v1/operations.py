7018b54f656b57cb04476eb0d293ccb203fbc70e
Timur YodZ0 <tima-mustafa@mail.ru>
2024-07-27 02:56:32 +0600

    Create operations routes: add_one+get_all

--- backend/api/api_v1/operations.py
+++ backend/api/api_v1/operations.py
@@ -1,8 +1,26 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+from services.operations import OperationsService
+from core.schemas import OperationRead, OperationCreate
+from .dependencies import operations_service
 
 router = APIRouter()
 
 
 @router.get('')
-async def get_operations_list():
-    pass
+async def get_operations_list(
+        service: Annotated[OperationsService, Depends(operations_service)],
+) -> list[OperationRead]:
+    operations = await service.get_operations()
+    return operations
+
+
+@router.post('')
+async def add_new_operation(
+        operation: OperationCreate,
+        service: Annotated[OperationsService, Depends(operations_service)],
+):
+    operation_id = await service.add_operation(operation)
+    return {'operation_id': operation_id}
