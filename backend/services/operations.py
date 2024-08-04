49232b7a15d261ee0ac33d516d8a8c8fc0f6952b
Timur YodZ0 <tima-mustafa@mail.ru>
2024-08-05 01:28:00 +0600

    Add filtering to OperationService

--- backend/services/operations.py
+++ backend/services/operations.py
@@ -1,4 +1,4 @@
-from core.schemas import OperationCreate
+from core.schemas import OperationCreate, OperationFilter
 from utils.unit_of_work import UnitOfWork
 
 
@@ -12,7 +12,24 @@ class OperationsService:
             return operation_id
 
     @staticmethod
-    async def get_operations(uow: UnitOfWork):
+    async def get_all_operations(uow: UnitOfWork, offset: int = None, limit: int = None):
         async with uow:
-            operations = await uow.operations.find_all()
+            operations = await uow.operations.find_all(offset=offset, limit=limit)
+            return operations
+
+    @staticmethod
+    async def filter_operations(uow: UnitOfWork, filters: OperationFilter = None):
+        filters_dict = filters.model_dump()
+        currency = filters_dict['currency']
+
+        if filters_dict['date_start'] is not None:
+            period = {
+                'start': filters_dict['date_start'],
+                'end': filters_dict['date_end'],
+            }
+        else:
+            period = None
+        kind = filters_dict['kind']
+        async with uow:
+            operations = await uow.operations.filter_all(currency=currency, period=period, kind=kind)
             return operations
