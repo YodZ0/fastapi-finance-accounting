df2b0a340d6f449f6e6e7981e5d87c97f469be3c
Timur YodZ0 <tima-mustafa@mail.ru>
2024-08-05 01:30:22 +0600

    Add endpoints to get+filter operations

--- backend/api/api_v1/operations.py
+++ backend/api/api_v1/operations.py
@@ -1,24 +1,45 @@
 from fastapi import APIRouter
 
 from services.operations import OperationsService
-from core.schemas import OperationRead, OperationCreate
+from core.schemas import OperationRead, OperationCreate, OperationFilter
 from .dependencies import UOWDep
 
+# api_v1 url = ..api/v1/operations/
 router = APIRouter()
-
-
-@router.get('')
-async def get_operations(
-        uow: UOWDep,
-) -> list[OperationRead]:
-    operations = await OperationsService().get_operations(uow)
-    return operations
 
 
 @router.post('')
 async def add_operation(
-        operation: OperationCreate,
         uow: UOWDep,
+        operation: OperationCreate,
 ) -> dict:
     operation_id = await OperationsService().add_operation(uow, operation)
     return {'operation_id': str(operation_id), 'status': '201', 'message': 'Created'}
+
+
+@router.get('')
+async def get_all_operations(
+        uow: UOWDep,
+        offset: int = None,
+        limit: int = None,
+) -> list[OperationRead]:
+    operations = await OperationsService().get_all_operations(uow, offset=offset, limit=limit)
+    return operations
+
+
+@router.get('/filter')
+async def filter_operations(
+        uow: UOWDep,
+        currency: str = None,
+        kind: str = None,
+        date_start: str = None,
+        date_end: str = None,
+):
+    filters = OperationFilter(
+        currency=currency,
+        kind=kind,
+        date_start=date_start,
+        date_end=date_end
+    )
+    operations = await OperationsService().filter_operations(uow, filters)
+    return operations
