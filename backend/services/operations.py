68550a21080f7a94ca936314fe968c0ef8b2f50b
Timur YodZ0 <tima-mustafa@mail.ru>
2024-08-13 03:33:14 +0600

    Add delete+delete-multiple to OperationsService

--- backend/services/operations.py
+++ backend/services/operations.py
@@ -12,9 +12,23 @@ class OperationsService:
             return operation_id
 
     @staticmethod
-    async def get_all_operations(uow: UnitOfWork, offset: int = None, limit: int = None):
+    async def delete_operation(uow: UnitOfWork, operation_id: int) -> int | None:
         async with uow:
-            operations = await uow.operations.find_all(offset=offset, limit=limit)
+            operation_id = await uow.operations.delete_one(_id=operation_id)
+            await uow.commit()
+            return operation_id
+
+    @staticmethod
+    async def delete_multiple_operations(uow: UnitOfWork, operations_ids: list[int]) -> list[int]:
+        async with uow:
+            operations_ids = await uow.operations.delete_multiple(ids=operations_ids)
+            await uow.commit()
+            return operations_ids
+
+    @staticmethod
+    async def get_all_operations(uow: UnitOfWork, limit: int = None, offset: int = None):
+        async with uow:
+            operations = await uow.operations.find_all(limit=limit, offset=offset)
             return operations
 
     @staticmethod
