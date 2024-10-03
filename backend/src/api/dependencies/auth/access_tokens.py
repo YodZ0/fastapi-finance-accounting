4eb0dda658cae72dc4825e4dd698637d21db6974
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-03 13:28:36 +0600

    Add db_helper dependency

--- backend/src/api/dependencies/auth/access_tokens.py
+++ backend/src/api/dependencies/auth/access_tokens.py
@@ -3,9 +3,11 @@ from typing import TYPE_CHECKING, Annotated
 from fastapi import Depends
 
 from src.core.models import db_helper, AccessToken
+from src.loggers import get_logger
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+logger = get_logger(__name__)
 
 
 async def get_access_tokens_db(
@@ -15,3 +17,5 @@ async def get_access_tokens_db(
     ],
 ):
     yield AccessToken.get_db(session)
+        logger.debug("ACCESS_TOKEN: Session CALLED")
+        logger.debug("ACCESS_TOKEN: Session CLOSED.")
--- backend/src/api/dependencies/auth/users.py
+++ backend/src/api/dependencies/auth/users.py
@@ -3,15 +3,21 @@ from typing import TYPE_CHECKING, Annotated
 from fastapi import Depends
 
 from src.core.models import db_helper, User
+from src.core.models import User
+from src.core.models.database import get_db_helper
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from src.core.models.database import DataBaseHelper
+
 
 
 async def get_users_db(
-    session: Annotated[
-        "AsyncSession",
-        Depends(db_helper.session_getter),
+    db_helper: Annotated[
+        "DataBaseHelper",
+        Depends(get_db_helper),
     ],
 ):
     yield User.get_db(session)
+    async with db_helper.session_factory() as session:
+        yield User.get_db(session)
