48496d7337e3179512addf8b4b9c516711de927f
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-16 05:54:35 +0600

    Delete username field

--- backend/src/core/schemas/user.py
+++ backend/src/core/schemas/user.py
@@ -1,6 +1,5 @@
 import uuid
 import datetime
-from typing import Optional
 
 from fastapi_users import schemas
 from pydantic import EmailStr
@@ -8,7 +7,6 @@ from pydantic import EmailStr
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     id: uuid.UUID
-    username: str
     email: EmailStr
     created_at: datetime.date
 
@@ -18,9 +16,8 @@ class UserRead(schemas.BaseUser[uuid.UUID]):
 
 
 class UserCreate(schemas.BaseUserCreate):
-    username: str
     created_at: datetime.date = datetime.date.today()
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    username: Optional[str] = None
+    pass
