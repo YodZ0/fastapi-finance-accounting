452e84425a6ea669ac41ccf834f24db70357f002
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-16 15:48:33 +0600

    Add pydantic validation

--- backend/src/services/period.py
+++ backend/src/services/period.py
@@ -13,8 +13,7 @@ class PeriodService:
         user: User,
     ) -> dict[str, int]:
         period_dict = new_period.model_dump()
-        user_id = user.id
-        period_dict["user_id"] = user_id
+        period_dict["user_id"] = user.id
         try:
             async with uow:
                 new_period_id = await uow.periods.add_one(data=period_dict)
@@ -29,14 +28,34 @@ class PeriodService:
         uow: UnitOfWork,
         user: User,
     ) -> dict[str, list[Period]]:
-        user_id = user.id
         try:
             async with uow:
                 periods: list[Period] = await uow.periods.get_all_filtered(
-                    user_id=user_id,
+                    user_id=user.id,
                 )
-                return {"periods": periods}
+                result = [Period.model_validate(period) for period in periods]
+                return {"periods": result}
         except IntegrityError:
             raise
+        except Exception:
+            raise
+
+    @staticmethod
+    async def delete_period(
+        uow: UnitOfWork,
+        period_id: int,
+        user: User,
+    ) -> dict[str, int] | None:
+        try:
+            async with uow:
+                period: Period = await uow.periods.get_one_filtered(
+                    id=period_id,
+                    user=user.id,
+                )
+                if period:
+                    deleted_period_id = await uow.periods.delete_one(_id=period.id)
+                    return {"deleted_id": deleted_period_id}
+                else:
+                    return None
         except Exception:
             raise
