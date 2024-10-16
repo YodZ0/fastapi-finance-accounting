f58d7a18d6ddd4a9b4a1e48acc60ddd5f7238ffc
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-16 15:52:05 +0600

    Change test queue + add more assertions

--- backend/tests/functional/categories/test_categories.py
+++ backend/tests/functional/categories/test_categories.py
@@ -3,19 +3,9 @@ from httpx import AsyncClient
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_categories_get_all_router(aclient: AsyncClient, user_token: str):
-    response = await aclient.get(
-        "/categories/all",
-        headers={"Authorization": user_token},
-    )
-
-    assert response.status_code == 200
-
-
-@pytest.mark.asyncio(loop_scope="session")
 async def test_category_add_router(aclient: AsyncClient, admin_token):
     new_category = {
-        "name": "test-category",
+        "name": "expense-category",
         "type": "EXPENSE",
     }
     response = await aclient.post(
@@ -28,3 +18,18 @@ async def test_category_add_router(aclient: AsyncClient, admin_token):
     assert response.status_code == 201
     assert data.get("message") == "success"
     assert data.get("data").get("new_category_id") == 1
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_categories_get_all_router(aclient: AsyncClient, user_token: str):
+    response = await aclient.get(
+        "/categories/all",
+        headers={"Authorization": user_token},
+    )
+    res = response.json()
+    data = res.get("data")
+    categories = data.get("categories")
+
+    assert response.status_code == 200
+    assert data is not None
+    assert len(categories.get("expenses")) == 1
