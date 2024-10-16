146c7af6d17fc1137b2d8421c5379641b7b3ebce
Timur YodZ0 <tima-mustafa@mail.ru>
2024-10-16 15:41:13 +0600

    Create Operation model

--- backend/src/core/models/__init__.py
+++ backend/src/core/models/__init__.py
@@ -4,6 +4,7 @@ __all__ = [
     "AccessToken",
     "ORMCategory",
     "ORMPeriod",
+    "ORMOperation",
 ]
 
 from .base import Base
@@ -12,3 +13,4 @@ from .user import User
 from .access_token import AccessToken
 from .category import ORMCategory
 from .period import ORMPeriod
+from .operation import ORMOperation
